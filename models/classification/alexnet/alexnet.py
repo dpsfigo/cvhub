e2e6b48a0c2fb9fe689c07c1b329ef3e21affc73
dpsfigo <dps_figo@hotmail.com>
2023-07-07 18:08:19 +0800

    使用torchvision自带的alexnet网络结构

--- models/classification/alexnet/alexnet.py
+++ models/classification/alexnet/alexnet.py
@@ -2,49 +2,86 @@
 Author: dpsfigo
 Date: 2023-06-27 15:40:01
 LastEditors: dpsfigo
-LastEditTime: 2023-07-05 14:28:37
+LastEditTime: 2023-07-07 18:06:12
 Description: AlexNet backbone
 '''
 import torch
 import torch.nn as nn
 
+# class AlexNet(nn.Module):
+#     def __init__ (self, num_classes):
+#         super().__init__()
+#         self.num_classes = num_classes
+#         self.features = nn.Sequential(
+#             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+#             nn.ReLU(inplace=True),
+#             nn.MaxPool2d(kernel_size=3, stride=2),
+
+#             nn.Conv2d(64, 192, kernel_size=5, padding=2),
+#             nn.ReLU(inplace=True),
+#             nn.MaxPool2d(kernel_size=3, stride=2),
+
+#             nn.Conv2d(192, 384, kernel_size=3, padding=1),
+#             nn.ReLU(inplace=True),
+
+#             nn.Conv2d(384, 256, kernel_size=3, padding=1),
+#             nn.ReLU(inplace=True),
+            
+#             nn.Conv2d(256, 256, kernel_size=3, padding=1),
+#             nn.ReLU(inplace=True),
+#             nn.MaxPool2d(kernel_size=3, stride=2),
+#         )
+#         self.classifier = nn.Sequential(
+#             nn.Dropout(),
+#             nn.Linear(256*6*6, 4096),
+#             nn.ReLU(inplace=True),
+#             nn.Dropout(),
+#             nn.Linear(4096, 4096),
+#             nn.ReLU(inplace=True),
+#             nn.Linear(4096, num_classes)
+#         )
+    
+#     def forward(self, x:torch.Tensor)->torch.Tensor:
+#         x = x.view(-1,3,224,224)
+#         x = self.features(x)
+#         if self.num_classes > 0:
+#             x = x.view(x.size(0), 256*6*6)
+#             x = self.classifier(x)
+#         return x
+
 class AlexNet(nn.Module):
-    def __init__ (self, num_classes):
+    def __init__(self, num_classes: int = 1000, dropout: float = 0.5) -> None:
         super().__init__()
-        self.num_classes = num_classes
+        # _log_api_usage_once(self)
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
-
             nn.Conv2d(64, 192, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
-
             nn.Conv2d(192, 384, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-
             nn.Conv2d(384, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256*6*6, 4096),
+            nn.Dropout(p=dropout),
+            nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
-            nn.Dropout(),
+            nn.Dropout(p=dropout),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes)
+            nn.Linear(4096, num_classes),
         )
-    
-    def forward(self, x:torch.Tensor)->torch.Tensor:
-        x = x.view(-1,3,224,224)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.features(x)
-        if self.num_classes > 0:
-            x = x.view(x.size(0), 256*6*6)
-            x = self.classifier(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
         return x
